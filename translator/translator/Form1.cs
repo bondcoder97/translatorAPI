67c115d04626e729b5898b4cdde6cabba647017a
Andrey <andruha3097@gmail.com>
2018-09-17 22:47:23 +0400

    translator

---
+++ translator/translator/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Web;
+using Newtonsoft.Json;
+using System.Net;
+using System.IO;
+
+
+
+namespace translator
+{
+    public partial class Form1 : Form
+    {
+         string url="https://translate.yandex.net/api/v1.5/tr.json/translate?key=yourKey&text=";
+         UriTypeConverter myWord;
+        
+       
+        public Form1()
+        {
+            InitializeComponent();
+            myWord= new UriTypeConverter() ;
+           
+        }
+
+    
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+       
+        string changedUrl = "";
+
+            changedUrl=url;
+            var word = textBox1.Text;
+             
+            changedUrl+= myWord.ConvertFrom(word);
+
+            changedUrl+="&lang=en";
+            
+
+
+          string json = new WebClient().DownloadString(changedUrl);
+          Translate obj = JsonConvert.DeserializeObject<Translate>(json);
+         
+            
+                textBox2.Text = obj.text[0];
+
+                
+
+
+                
+                
+            
+            changedUrl=url;
+       
+        
+        
+        
+        
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+        }
+
+
+
+
+
+
+    }
+}
---
+++ translator/translator/Translate.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace translator
+{
+    class Translate
+    {
+        public string code { get; set; }
+        public string lang { get; set; }
+        public string[] text;
+    }
+}
